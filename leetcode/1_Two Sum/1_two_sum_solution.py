e932e0daab6542a94c52acb7f438bd6a2306faf7
serge <sergejyurskyj@yahoo.com>
2023-05-27 12:28:28 -0700

    Initial commit

---
+++ leetcode/1_Two Sum/1_two_sum_solution.py
@@ -0,0 +1,40 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    @staticmethod
+    def twoSum(nums: List[int], target: int) -> List[int]:
+        seen = defaultdict(int)
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return []
+
+
+class TestSolution:
+    @staticmethod
+    def run_tests():
+        test_cases = [
+            ([2, 7, 11, 15], 9, [0, 1]),
+            ([3, 2, 4], 6, [1, 2]),
+            ([3, 3], 6, [0, 1]),
+        ]
+
+        for nums, target, expected in test_cases:
+            result = Solution.twoSum(nums, target)
+            assert result == expected, f"Expected: {expected}, Got: {result}"
+            result = Solution.twoSum(nums, target)
+            print(f"Input: nums={nums}, target={target}")
+            print("Result:", result)
+            print("Expected output:", expected)
+            print("Test passed:", result == expected)
+            print()
+
+        print("All test cases passed!")
+
+
+if __name__ == "__main__":
+    TestSolution.run_tests()
