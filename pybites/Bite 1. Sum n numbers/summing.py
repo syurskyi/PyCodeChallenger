394b64177906cda0376e24991d7172fac1ba9221
serge <sergejyurskyj@yahoo.com>
2023-05-27 17:59:21 -0700

    Commited Bite 1. Sum n numbers working on the PyBite pages

---
+++ pybites/Bite 1. Sum n numbers/summing.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+def sum_numbers(numbers: List[int] = None) -> int:
+    default_numbers = list(range(1, 101))
+    if numbers is None:
+        numbers = default_numbers
+    return sum(numbers)
